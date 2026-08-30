84b590a660d89eff3474f1ef8f59381ec4ce647f
J05ue21 <orellana3566@gmail.com>
2026-08-29 21:27:01 -0600

    implementar: llamada de instancia de AuthRepository, simulacion de Autenticacion

--- app/src/main/java/com/example/myapplicationp/LoginActivity.kt
+++ app/src/main/java/com/example/myapplicationp/LoginActivity.kt
@@ -3,13 +3,17 @@ package com.example.myapplicationp
 import android.content.Intent
 import android.graphics.Paint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplicationp.data.AuthRepository
 import com.example.myapplicationp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var sessionManager: SessionManager
+
+    private val authRepository = AuthRepository() //instanciando AuthRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,17 +24,17 @@ class LoginActivity : AppCompatActivity() {
 
         sessionManager = SessionManager(this)
 
-        // 1. REQUISITO AUTOLOGIN: Si ya inició sesión antes, se salta el Login automáticamente
+        // AUTOLOGIN: Si ya inició sesión antes, se salta el Login automáticamente
         if (sessionManager.isLoggedIn()) {
             irAMainActivity()
             return
         }
 
-        // 2. REQUISITO RECORDAR DATOS: Si el checkbox estaba activo, autocompleta los campos
+        // rECORDAR DATOS: Si el checkbox estaba activo, autocompleta los campos
         if (sessionManager.isRememberMeActive()) {
             binding.etEmail.setText(sessionManager.getSavedUsername())
             binding.etPassword.setText(sessionManager.getSavedPassword())
-            binding.cbRemember.isChecked = true // Asegúrate de que el id de tu CheckBox en el XML sea cbRemember
+            binding.cbRemember.isChecked = true
         }
 
         // Subrayar el texto de "Crear usuario" para que se vea como enlace
@@ -40,6 +44,39 @@ class LoginActivity : AppCompatActivity() {
         binding.btnLogin.setOnClickListener {
             validarEntradas()
         }
+
+        binding.btnGoogle.setOnClickListener {
+            authRepository.handleGoogleSignInResult("token_de_google_123") { exito, mensaje ->
+                Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+                if (exito) {
+                    sessionManager.saveSession("usuario.google@gmail.com", "123456", false)
+                    irAMainActivity()
+                }
+            }
+        }
+
+        // Botón GitHub
+        binding.btnGithub.setOnClickListener {
+            authRepository.handleGitHubSignIn { exito, mensaje ->
+                Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+                if (exito) {
+                    sessionManager.saveSession("usuario_de_github", "123456", false)
+                    irAMainActivity()
+                }
+            }
+        }
+
+        // Botón Outlook
+        binding.btnOutlook.setOnClickListener {
+            authRepository.handleOutlookSignIn { exito, mensaje ->
+                Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+                if (exito) {
+                    sessionManager.saveSession("usuario.outlook@outlook.com", "123456", false)
+                    irAMainActivity()
+                }
+            }
+        }
+
     }
 
     private fun validarEntradas() {
@@ -47,21 +84,21 @@ class LoginActivity : AppCompatActivity() {
         val password = binding.etPassword.text.toString().trim()
         val recordarStatus = binding.cbRemember.isChecked // Estado del checkbox
 
-        // 1. Validar que ponga cualquier nombre (que no esté vacío)
+        // validar que ponga cualquier nombre (que no esté vacío)
         if (usuario.isEmpty()) {
             binding.etEmail.error = "Ingresa tu usuario"
             binding.etEmail.requestFocus()
             return
         }
 
-        // 2. Validar que ponga una contraseña válida
+        // validar que ponga una contraseña válida
         if (password.isEmpty() || password.length < 6) {
             binding.etPassword.error = "La contraseña debe tener al menos 6 caracteres"
             binding.etPassword.requestFocus()
             return
         }
 
-        // 3. REQUISITO PERSISTENCIA: Si pasa las validaciones, guarda los datos localmente
+        // PERSISTENCIA: Si pasa las validaciones, guarda los datos localmente
         sessionManager.saveSession(usuario, password, recordarStatus)
 
         // Navegar al Menú Principal (MainActivity)
